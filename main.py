b02c4cc2fcfb6f0f0b2b5cec7f7000f81da37ae0
Angelo Rios <angelorios@live.com>
2024-12-23 16:40:59 +0100

    ref: improve win check

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import random
 
-
-game_running: bool = True
-current_player: str = "X"
-winner: str = None
-
-
 # Create a new board
 def create_board():
     board = [([" "] * 3) for _ in range(3)]
@@ -45,43 +39,6 @@ def get_empty(board: list) -> list:
                 empty_cells.append(num)
             num += 1
     return empty_cells
-
-
-# check rows
-def check_rows(board: list) -> bool:
-    global winner
-    if board[0][0] == board[0][1] == board[0][2] and board[0][0] != " ":
-        winner = board[0][0]
-        return True
-    elif board[1][0] == board[1][1] == board[1][2] and board[1][0] != " ":
-        winner = board[1][0]
-        return True
-    elif board[2][0] == board[2][1] == board[2][2] and board[2][0] != " ":
-        winner = board[2][0]
-        return True
-
-
-def check_columns(board: list) -> bool:
-    global winner
-    if board[0][0] == board[1][0] == board[2][0] and board[0][0] != " ":
-        winner = board[0][0]
-        return True
-    if board[0][1] == board[1][1] == board[2][1] and board[0][1] != " ":
-        winner = board[1][0]
-        return True
-    if board[0][2] == board[1][2] == board[2][2] and board[0][2] != " ":
-        winner = board[2][0]
-        return True
-
-
-def check_diagonals(board: list) -> bool:
-    global winner
-    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
-        winner = board[0][0]
-        return True
-    if board[0][2] == board[1][1] == board[2][0] and board[2][0] != " ":
-        winner = board[2][0]
-        return True
     
 
 # Check Tie
@@ -91,14 +48,29 @@ def check_tie(board: list):
         return True
 
 
-# Game check
-def check_status(board: list):
-    global game_running, winner
-    if check_rows(board) or check_columns(board) or check_diagonals(board):
-        print(f"We have a winner: {winner}!")
-        game_running = False
-    elif check_tie(board):
-        game_running = False
+# Check for winn
+def check_status(board: list, player: str) -> True:
+    options = [
+        [(0, 0), (0, 1), (0, 2)],
+        [(1, 0), (1, 1), (1, 2)],
+        [(2, 0), (2, 1), (2, 2)],
+        [(0, 0), (1, 0), (2, 0)],
+        [(0, 1), (1, 1), (2, 1)],
+        [(0, 2), (1, 2), (2, 2)],
+        [(0, 0), (1, 1), (2, 2)],
+        [(0, 2), (1, 1), (2, 0)],
+    ]
+    for option in options:
+        win = True
+        for each in option:
+            row, col = each
+            if board[row][col] != player:
+                win = False
+                break
+        if win:
+            return True
+    return False
+
 
 # Make a move
 def make_move(move: tuple, board: list) -> None:
@@ -124,25 +96,26 @@ def computer_move(board: list) -> None:
     empty_cells: list = get_empty(board)
     c_move: int = int(random.choice(empty_cells)) -1, "O"
     make_move(c_move, board)
-    print(f"Computer: {c_move[0]+1}")
-
-
-# Switch player
-def switch_player(board):
-    global current_player
-    if current_player == "X":
-        human_move(board)
-        check_status(board)
-        current_player = "O"
-    else:
-        computer_move(board)
-        check_status(board)
-        current_player = "X"
+    print(f"Computer choose: {c_move[0]+1}")
 
 
 table = create_board()
-while game_running:
+while True:
 
-    switch_player(table)
-
+    human_move(table)
     show_board(table)
+    if check_status(table, "X"):
+        print("You are the winner".upper())
+        break
+    
+    if check_tie(table):
+        break
+
+    computer_move(table)
+    show_board(table)
+    if check_status(table, "O"):
+        print("You lost! Computer wins.".upper())
+        break
+
+    if check_tie(table):
+        break
