fa345e2e0ef7eb45a3e088cbb4a2e00dfb6a52b0
Angelo Rios <angelorios@live.com>
2025-01-24 17:15:30 +0100

    fix: caso de prueba get_empty_spots

--- tests/test_get_empty.py
+++ tests/test_get_empty.py
@@ -1,16 +1,24 @@
 import unittest
-from functions.check import get_empty
+from game_fn.tictactoe import TicTacToe
 
 class testGetEmpty(unittest.TestCase):
     def setUp(self):
-        self.board = [
+        """ Prueba de la funcion """
+        # Crear instancia del juego para llamar funcion
+        # get_empty_spots()
+
+        self.game = TicTacToe()
+
+        # Definicion del tablero de pruebas
+        self.game.board = [
             ["X", "O", " "],
             ["X", " ", "O"],
             ["O", "X", " "]
         ]
-        self.expected_empty = [3, 5, 9]
+        self.expected_empty = [2, 4, 8]
 
-    def test_get_empty_cells(self):
-        results = get_empty(self.board)
+    def test_get_empty_spots(self):
+        """Ejecutar caso de prueba"""
+        results = self.game.get_empty_spots()
         self.assertEqual(results, self.expected_empty)
 
